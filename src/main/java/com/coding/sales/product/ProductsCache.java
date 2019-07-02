f7e6334a7967738f9cfb780f72088550f2fcc6d4
LittleBell123 <415725257@qq.com>
2019-07-02 21:02:47 +0800

    完善产品信息列表

--- src/main/java/com/coding/sales/product/ProductsCache.java
+++ src/main/java/com/coding/sales/product/ProductsCache.java
@@ -14,8 +14,13 @@ public class ProductsCache {
 	public static Map<String , Product> products = new HashMap<String, Product>();
 
 	static{
-		products.put("001001", new Product("世园会五十国钱币册", "001001", ProductItemConstant.ProductUnit.PRD_UNIT_VOLUM, 1080.00, null , null));
+		products.put("001001", new Product("世园会五十国钱币册", "001001", ProductItemConstant.ProductUnit.PRD_UNIT_VOLUM, 998.00, null , null));
 		products.put("001002", new Product("2019北京世园会纪念银章大全40g", "001002", ProductItemConstant.ProductUnit.PRD_UNIT_BOX, 1380.00, "可使用9折打折券" , null));
+		products.put("003001", new Product("招财进宝", "003001", ProductItemConstant.ProductUnit.PRD_UNIT_A, 1580.00, "可使用95折打折券" , null));
+		products.put("003002", new Product("水晶之恋", "003002", ProductItemConstant.ProductUnit.PRD_UNIT_A, 980.00, null, "第3件半价，满3送1"));
+		products.put("002002", new Product("中国经典钱币套装", "002002", ProductItemConstant.ProductUnit.PRD_UNIT_SET, 998.00, null , "每满2000减30，每满1000减10"));
+		products.put("002001", new Product("守扩之羽比翼双飞4.8g", "002001", ProductItemConstant.ProductUnit.PRD_UNIT_A, 1080.00, "可使用95折打折券" , "第3件半价，满3送1"));
+		products.put("002003", new Product("中国银象棋12g", "002003", ProductItemConstant.ProductUnit.PRD_UNIT_SET, 698.00, "可使用9折打折券" , "每满3000元减350, 每满2000减30，每满1000减10"));
 	}
 	
 	public static Map<String, String> FULL_SUB_RULE = new HashMap<String, String>();
