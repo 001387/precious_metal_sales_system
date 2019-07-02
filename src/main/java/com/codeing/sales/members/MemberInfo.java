ed6aa206529a850a494dc8ca4a9ebd7fed8c603b
LittleBell123 <415725257@qq.com>
2019-07-02 17:02:04 +0800

    给会员信息设置get和set的方法

--- src/main/java/com/codeing/sales/members/MemberInfo.java
+++ src/main/java/com/codeing/sales/members/MemberInfo.java
@@ -1,8 +1,40 @@
 package com.codeing.sales.members;
 
 public class MemberInfo {
-	public String name;
-	public String level;
-	public String cardId;
-	public String bonusPoints;
+	private String level;
+	private String cardId;
+	private String bonusPoints;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+
+	public String getBonusPoints() {
+		return bonusPoints;
+	}
+
+	public void setBonusPoints(String bonusPoints) {
+		this.bonusPoints = bonusPoints;
+	}
 }
