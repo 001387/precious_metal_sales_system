2b491d6845662d346106424c75ffd1f6526f8e23
yz303215 <web_yang@foxmail.com>
2019-07-02 19:11:13 +0800

    member方法初始化信息添加static

--- src/main/java/com/coding/sales/members/Members.java
+++ src/main/java/com/coding/sales/members/Members.java
@@ -1,14 +1,12 @@
 package com.coding.sales.members;
 
-import java.lang.reflect.Member;
 import java.util.HashMap;
 
-import org.omg.CORBA.PRIVATE_MEMBER;
 
 public class Members {
 	public static HashMap<String, MemberInfo> members = new HashMap<String, MemberInfo>();
 	
-	{
+	static{
 		members.put("6236609999", new MemberInfo("马丁", "普卡", "6236609999", 9860));
 		members.put("6236609999", new MemberInfo("王立", "金卡", "6630009999", 48860));
 		members.put("6236609999", new MemberInfo("李想", "白金卡", "8230009999", 98860));
